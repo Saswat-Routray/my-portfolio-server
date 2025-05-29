a71aa494dac1c0b2b3f6db35bd782888ed6f9095
Saswat Routray <saswat.routray92m@gmail.com>
2025-05-30 01:08:17 +0530

    finalize

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "my-portfolio-production-ae96.up.railway.app",
     methods: ["POST"],
 }));
 
